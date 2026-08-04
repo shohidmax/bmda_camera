91d4002b963fdf2e445a7073484c5f7014c1ce7a
shohid max <shohidmax@gmail.com>
2026-08-05 03:49:54 +0600

    fi aut

--- Dashbord/src/utils/firebase.ts
+++ Dashbord/src/utils/firebase.ts
@@ -11,12 +11,13 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDSqcpsRq64j4LbkmeOjeUIpa91HlFpZvc",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "camerabb-5fdd9.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "camerabb-5fdd9",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "camerabb-5fdd9.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1067165632500",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1067165632500:web:b93fe67a9fc3b60d081ba1",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-FZC29VRVC3"
 };
 
 const hasFirebaseKeys = 
